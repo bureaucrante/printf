60da8b59c4568a3cc0fa074114d00230d718d794
robelyes <robelyeshiwas93@gmail.com>
2022-10-18 12:12:21 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,80 +1,44 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
+ * _printf - printf function
+ * @format: const char pointer
+ * Return: b_len
  */
-
-int (*_print_form(const char *format))(va_list)
-{
-	int i = 0;
-	p_type p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"d", print_d},
-		{"i", print_d},
-		{"u", print_unsigned},
-		{"%", print_per},
-		{NULL, NULL}
-	};
-
-	while (p[i].cc)
-	{
-		if (*p[i].cc == *format)
-			break;
-		i++;
-	}
-	return (p[i].f);
-}
 
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, count = 0;
-	va_list args;
-	int(*f)(va_list);
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	if (!format)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
-	va_start(args, format);
-
-	while (format[i])
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if (format[i] == '%')
+		if (*p == '%')
 		{
-			i++;
-
-			/*while (format[i] == ' ')
+			p++;
+			if (*p == '%')
 			{
-				i++;
-			}*/
-
-			f = _print_form(&format[i]);
-
-			if (f)
-			{
-				count += f(args);
-				i += 1;
+				count += _putchar('%');
 				continue;
 			}
-			i++;
-
-			if (format[i] != '\0')
-			{
-				if (format[i] == ' ')
-				{
-					_putchar(format[i]);
-				}
-			}
-		}
-
-		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		i++;
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-
-	va_end(args);
+	_putchar(-1);
+	va_end(arguments);
 	return (count);
 }
