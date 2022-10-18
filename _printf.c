bde04551e15969e0d3ae72b23071e0eeee2da503
bureaucrante <schwinston00@gmail.com>
2022-10-18 16:59:30 -0700

    ff

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _printf - prints output according to format
+ * @format: pointer to arguments.
+ * Return: number of printed characters.
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list args;
+	int (*f)(va_list);
+
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+
+			f = _print_form(&format[i]);
+			if (f)
+			{
+				count += f(args);
+				i += 1;
+				continue;
+			}
+			i++;
+
+			if (format[i] != '\0')
+			{
+				if (format[i] == ' ')
+					_putchar(format[i]);
+			}
+		}
+
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
