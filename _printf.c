1a6bdaf8a0b3891b684fd5db0fa96fd2b0d4e426
bureaucrante <schwinston00@gmail.com>
2022-10-18 17:07:21 -0700

    checking checks

--- _printf.c
+++ _printf.c
@@ -13,10 +13,12 @@ int _printf(const char *format, ...)
 	va_list args;
 	int (*f)(va_list);
 
-	if (!format)
-		return (-1);
-
 	va_start(args, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 
 	while (format[i])
 	{
